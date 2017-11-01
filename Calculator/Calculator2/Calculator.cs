5545749db77424edc3c63217eb0395aae7a77c6f
jesper0438 <robr0002@hz.nl>
2017-11-01 14:18:59 +0100

    First Class implemented

---
+++ Calculator/Calculator2/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class Calculator : CalculatorMusthaves
+    {
+        public int Diff(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Product(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Quotient(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sum(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
